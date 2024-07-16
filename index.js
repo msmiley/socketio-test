ae3d45a1eb1ea6531d7ec9e54aaad0a8362a72c0
Mike Smiley <msmiley@alumni.rice.edu>
2024-07-16 11:18:15 -0500

    add any handler

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { io } from 'socket.io-client';
 
 const HOST = 'localhost';
-const PORT = 5000;
-const NAMESPACE = 'viv';
+const PORT = 5500;
+const NAMESPACE = '';
 
 const socket = io(`http://${HOST}:${PORT}/${NAMESPACE}`, {
   path: `/socket.io`,  // handshake path
@@ -12,7 +12,11 @@ const socket = io(`http://${HOST}:${PORT}/${NAMESPACE}`, {
 console.log('socket.io-test')
 
 socket.on('connect', () => {
-  console.log('connected', socket.id)
+  console.log('connected, id=', socket.id)
+});
+
+socket.onAny((event, ...args) => {
+  console.log('received event:', event, args);
 });
 
 socket.on("disconnect", () => {
